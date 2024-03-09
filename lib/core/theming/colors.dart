dd20e76e695daae03984918e1ccecceaaff9b52b
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-03-09 22:28:53 +0200

    added colors App

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class ColorsApp {
+  
+  static const Color mainGreen = Color(0xFF5FD068);
+
+  static const Color lightGreen = Color(0xFFF9FFF6);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static Color black = const Color(0xFF000000);
+
+  static Color lightGray = const Color(0xFFD9D9D9);
+
+  static Color gray = const Color(0xFFB5B5B5);
+
+  static Color yallow = const Color(0xFFF5DF99);
+
+  static Color blackOpacity = const Color(0xFF000000).withOpacity(0.5);
+}
