adb9b0aacff8cfb6a18bc313ed0e42d544743cec
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-03-09 22:28:20 +0200

    added localization  project

--- lib/home_ease__app.dart
+++ lib/home_ease__app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:easy_localization/easy_localization.dart';
 
 class HomeEase extends StatelessWidget {
   const HomeEase({super.key});
@@ -6,11 +8,13 @@ class HomeEase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // localizationsDelegates: context.localizationDelegates,
+      // supportedLocales: context.supportedLocales,
+      // locale: context.locale,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:home_ease/home_ease__app.dart';
+// ignore: depend_on_referenced_packages
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(const HomeEase());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: const [Locale('en', 'US'), Locale('ar', 'EG')],
+        path:
+            'assets/translations', // <-- change the path of the translation files
+        fallbackLocale: const Locale('ar', 'EG'),
+        // assetLoader: const CodegenLoader(),
+        child: const HomeEase()),
+  );
 }
-
-
-
