53f61f518cb1417aa27fc82709950fcbe12aff8b
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-03-11 11:46:39 +0200

    added statusBarColor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:home_ease/home_ease__app.dart';
 // ignore: depend_on_referenced_packages
 import 'package:easy_localization/easy_localization.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+    statusBarIconBrightness:
+        Brightness.dark, //<-- For Android SEE HERE (dark icons)
+  ));
+
   WidgetsFlutterBinding.ensureInitialized();
   // await EasyLocalization.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
 
   runApp(
     EasyLocalization(
