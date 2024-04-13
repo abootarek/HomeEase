98e44b23c03d97268523464c1b06e8376cba02d9
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-04-13 16:25:45 +0200

    added android 12 splash

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'package:home_ease/home_ease__app.dart';
 import 'package:easy_localization/easy_localization.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() async {
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.white,
     statusBarIconBrightness:
