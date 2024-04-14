a1af681217840773d5b3923a6a22a7eb54fa700d
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-04-14 20:53:43 +0200

    added class FirebaseResult added LoginCubit added LoginState

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,16 @@ import 'package:home_ease/home_ease__app.dart';
 import 'package:easy_localization/easy_localization.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.white,
